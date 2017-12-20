969ecb61bbadb24b797f8c1c3ca93b269990b257
Nicholas Sauter <nksauter@lbl.gov>
2017-12-20 13:31:58 -0800

    K-series simulations, new value of k_sol=0.435

--- sim/util_fmodel.py
+++ sim/util_fmodel.py
@@ -1,5 +1,23 @@
 from __future__ import division
 
+def hisym_fcalc_from_pdb(resolution,pdb_text,algorithm=None,wavelength=0.9):
+  from iotbx import pdb
+  pdb_inp = pdb.input(source_info=None,lines = pdb_text)
+  xray_structure = pdb_inp.xray_structure_simple()
+  xray_structure.show_summary(prefix="Input structure ")
+  #
+  # take a detour to insist on calculating anomalous contribution of every atom
+  scatterers = xray_structure.scatterers()
+  for sc in scatterers:
+    from cctbx.eltbx import sasaki, henke
+    #expected_sasaki = sasaki.table(sc.element_symbol()).at_angstrom(wavelength)
+    expected_henke = henke.table(sc.element_symbol()).at_angstrom(wavelength)
+    sc.fp = expected_henke.fp()
+    sc.fdp = expected_henke.fdp()
+
+  fcalc = xray_structure.structure_factors(
+    d_min=resolution, anomalous_flag=True, algorithm=algorithm).f_calc()
+  return fcalc.amplitudes()
 
 def fcalc_from_pdb(resolution,pdb_text,algorithm=None,wavelength=0.9):
   from iotbx import pdb
@@ -50,7 +68,7 @@ def fmodel_from_pdb(resolution,pdb_text,algorithm=None,wavelength=0.9):
     # even if the observed unit cell differs slightly from the reference.
   params2.output.type = "complex"
   params2.high_resolution = resolution
-  params2.fmodel.k_sol = 0.35
+  params2.fmodel.k_sol = 0.435 # "Babinet" optimum, used 0.35 previously
   params2.fmodel.b_sol = 46.
   params2.structure_factors_accuracy.algorithm = algorithm
   import mmtbx
@@ -64,3 +82,43 @@ def fmodel_from_pdb(resolution,pdb_text,algorithm=None,wavelength=0.9):
   f_model_real.show_summary(prefix="FMODEL ")
   return f_model_real
 
+class gen_fmodel:
+  def __init__(self,resolution,pdb_text,algorithm=None,wavelength=0.9):
+
+    from iotbx import pdb
+    pdb_inp = pdb.input(source_info=None,lines = pdb_text)
+    xray_structure = pdb_inp.xray_structure_simple()
+    xray_structure.show_summary(prefix="Input structure ")
+    #
+    # take a detour to insist on calculating anomalous contribution of every atom
+    scatterers = xray_structure.scatterers()
+    for sc in scatterers:
+      from cctbx.eltbx import sasaki, henke
+      #expected_sasaki = sasaki.table(sc.element_symbol()).at_angstrom(wavelength)
+      expected_henke = henke.table(sc.element_symbol()).at_angstrom(wavelength)
+      sc.fp = expected_henke.fp()
+      sc.fdp = expected_henke.fdp()
+
+    import mmtbx.command_line.fmodel
+    phil2 = mmtbx.command_line.fmodel.fmodel_from_xray_structure_master_params
+    params2 = phil2.extract()
+    # adjust the cutoff of the generated intensities to assure that
+    # statistics will be reported to the desired high-resolution limit
+    # even if the observed unit cell differs slightly from the reference.
+    params2.output.type = "complex"
+    params2.high_resolution = resolution
+    params2.fmodel.k_sol = 0.35
+    params2.fmodel.b_sol = 46.
+    params2.structure_factors_accuracy.algorithm = algorithm
+    self.params2 = params2
+    self.xray_structure = xray_structure
+  def get_amplitudes(self):
+    import mmtbx
+    f_model_complex = mmtbx.utils.fmodel_from_xray_structure(
+      xray_structure = self.xray_structure,
+      f_obs          = None,
+      add_sigmas     = False,
+      params         = self.params2).f_model
+    f_model_real = abs(f_model_complex)
+    f_model_real.set_observation_type_xray_amplitude()
+    return f_model_real
