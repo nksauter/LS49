645f0f1407697efaaf8d320f3c9ca85c55656720
Nicholas Sauter <nksauter@lbl.gov>
2018-09-15 17:49:14 -0700

    One more test

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,7 @@ tst_list = (
   "$D/tests/tst_numpy_lsq.py",
   "$D/tests/tst_spectrum_iterator.py",
   "$D/tests/tst_input_pdb.py",
+  "$D/tests/tst_structure_factors.py",
   )
 
 def run_standalones():
@@ -16,3 +17,28 @@ def run_standalones():
 
 if (__name__ == "__main__"):
   run_standalones()
+
+""" Discussion.
+tst_numpy_lsq.py:
+  Shows that numpy least squares fit works properly.
+  Fits the relationship between spectral peak and xtc-recorded energy.
+  Numpy code is not thread safe; this workaround temporily sets OMP_NUM_THREADS to 1
+tst_spectrum_iterator.py:
+  sameness of processed spectra to reference run
+tst_input_pdb.py:
+  fetches PDB code 1m2a and checks identity to old reference file
+tst_structure_factors.py:
+  the computed structure factors without energy dependence, in both P1 and C2
+the computed structure factors at selected energies
+the mosaic domains
+the 100000 random orientations
+the standard-wavelength raw image
+a sample wavelength energy contribution to raw image
+the air and water scatters
+the final 100-channel image
+Make sure that I can test the original JH code as well!!!
+In other words, equivalence of add_nanobragg_spots() and add_nanobragg_spots_nks
+()
+Or alternatively, re-express my step5 problem in terms of his code.
+Write tests that replicate my Bayesian estimates.
+"""
---
+++ tests/tst_structure_factors.py
@@ -0,0 +1,51 @@
+from __future__ import division, print_function
+from six.moves import cPickle
+import os
+from LS49.spectra import generate_spectra
+
+ls49_big_data = os.environ["LS49_BIG_DATA"] # get absolute path from environment
+def get_results():
+  return cPickle.load(open(os.path.join(ls49_big_data,"data/spectra209.pickle"),"rb"))
+generate_spectra.get_results = get_results
+
+def get_pdb_lines():
+  return open(os.path.join(ls49_big_data,"1m2a.pdb"),"r").read()
+
+ls49_big_data = os.environ["LS49_BIG_DATA"] # get absolute path from environment
+
+def get_results():
+  R = cPickle.load(open(os.path.join(ls49_big_data,"data/spectra209.pickle"),"rb"))
+  return R
+
+def single_wavelength_fmodel(create):
+  from LS49.spectra.generate_spectra import spectra_simulation
+  SS = spectra_simulation()
+  iterator = SS.generate_recast_renormalized_images(20,energy=7120.,total_flux=1e12)
+  wavlen, flux, wavelength_A = next(iterator) # list of lambdas, list of fluxes, average wavelength
+
+  direct_algo_res_limit = 1.7
+  from LS49.sim.util_fmodel import gen_fmodel
+  for flag in [True,False]:
+    GF = gen_fmodel(resolution=direct_algo_res_limit,pdb_text=get_pdb_lines(),algorithm="fft",wavelength=wavelength_A)
+    GF.set_k_sol(0.435)
+    if flag:  GF.make_P1_primitive()
+    sfall_main = GF.get_amplitudes()
+    sfall_main.show_summary(prefix = "Amplitudes used ")
+    if create: # write the reference for the first time
+      cPickle.dump(sfall_main,
+      open(os.path.join(ls49_big_data,"reference","sf_reference_cb_to_P1_%s"%(str(flag))),"wb"),cPickle.HIGHEST_PROTOCOL)
+    else: # read the reference and assert sameness to sfall_main
+      sfall_ref = cPickle.load(open(os.path.join(ls49_big_data,"reference","sf_reference_cb_to_P1_%s"%(str(flag))),"rb"))
+      T = sfall_main; S = sfall_ref
+      assert S.space_group() == T.space_group()
+      assert S.unit_cell().parameters() == T.unit_cell().parameters()
+      assert S.indices() == T.indices(); assert S.data() == T.data()
+    print()
+
+# ideally, do a calculation to prove that the duplicated structure factors in the
+# P1 cell are equivalent to those in C2, and / or try to attribute the difference
+# to the bulk solvent model rather then the site scatterer model.
+
+if __name__=="__main__":
+  single_wavelength_fmodel(create=False)
+  print("OK")
