ea85c1e23a29040d242b678dcee5e9e8e0adcbf1
Nicholas Sauter <nksauter@lbl.gov>
2018-10-27 14:56:38 -0700

    Superset of all tests of interest to cctbx xfel at present.

---
+++ tests/superset-test-all.py
@@ -0,0 +1,35 @@
+# LIBTBX_SET_DISPATCHER_NAME cctbx_regression.test_all
+
+from __future__ import division
+from libtbx.command_line import run_tests_parallel
+import sys
+import libtbx.load_env # implicit import
+
+if (__name__ == "__main__") :
+  args = [
+    "module=libtbx",
+    "module=boost_adaptbx",
+    "module=scitbx",
+    "module=cctbx",
+    "module=iotbx",
+    "module=smtbx",
+    "module=spotfinder",
+    "module=rstbx",
+    "module=fable",
+    "module=simtbx",
+    "module=dxtbx",
+    "module=dials",
+    "module=annlib_adaptbx",
+    "module=cbflib_adaptbx",
+    "module=LS49",
+    "module=labelit",
+    "module=labelit_regression",
+    "module=dials_regression",
+    "module=xfel_regression",
+    #"module=mmtbx",
+    "module=cluster_regression",
+    "nproc=Auto",
+  ]
+
+  if (run_tests_parallel.run(args) > 0) :
+    sys.exit(1)
