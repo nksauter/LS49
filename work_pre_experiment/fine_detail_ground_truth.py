3b8c6be2733008a8e4351a9813c2a7ee92fe890c
Nicholas Sauter <nksauter@lbl.gov>
2019-01-20 09:41:26 -0800

    Add fine-detail ground truth. The randomized crystal orientation, taking into account the actual ensemble of 25 missetting matrices applied to the mosaic domains.

---
+++ work_pre_experiment/fine_detail_ground_truth.py
@@ -0,0 +1,114 @@
+from __future__ import division, absolute_import, print_function
+from scitbx.array_family import flex
+from scitbx.matrix import sqr,col
+from cctbx import crystal_orientation
+from simtbx.nanoBragg import nanoBragg
+import scitbx
+import math
+import os
+
+from LS49.sim import step5_pad
+from LS49.sim import step4_pad
+ls49_big_data = os.environ["LS49_BIG_DATA"] # get absolute path from environment
+step5_pad.big_data = ls49_big_data
+step4_pad.big_data = ls49_big_data
+from LS49.sim.util_fmodel import gen_fmodel
+from LS49.sim.step5_pad import data
+
+
+def tst_all(serial_no): #emulates the action of step5_pad.py in assigning a coarse orientation to each simulation event
+  mt = flex.mersenne_twister(seed=0)
+
+  Nimages = 100000
+  for iteration in range(Nimages):
+    rand_ori = sqr(mt.random_double_r3_rotation_matrix())
+    if serial_no == iteration:
+      return rand_ori
+  return None
+
+from LS49.work_pre_experiment.step5_ang_misset import get_items
+class nanoBragg_mock:
+ def __init__(self):
+
+  local_data = data()
+  direct_algo_res_limit = 1.7
+
+  wavelength_A = 1.3 # Angstroms, dummy value
+  GF = gen_fmodel(resolution=direct_algo_res_limit,pdb_text=local_data.get("pdb_lines"),algorithm="fft",wavelength=wavelength_A)
+  GF.set_k_sol(0.435)
+  GF.make_P1_primitive()
+  self.sfall_main = GF.get_amplitudes()
+
+  SIM = nanoBragg(detpixels_slowfast=(3000,3000),pixel_size_mm=0.11,Ncells_abc=(10,10,10),
+    # workaround for problem with wavelength array, specify it separately in constructor.
+    wavelength_A=wavelength_A,verbose=0)
+
+  SIM.mosaic_domains = 25  # 77 seconds.  With 100 energy points, 7700 seconds (2 hours) per image
+                           # 3000000 images would be 100000 hours on a 60-core machine (dials), or 11.4 years
+                           # using 2 nodes, 5.7 years.  Do this at SLAC? NERSC? combination of all?
+                           # SLAC downtimes: Tues Dec 5 (24 hrs), Mon Dec 11 (72 hrs), Mon Dec 18 light use, 24 days
+  SIM.mosaic_spread_deg = 0.05 # interpreted by UMAT_nm as a half-width stddev
+
+  UMAT_nm = flex.mat3_double()
+  mersenne_twister = flex.mersenne_twister(seed=0)
+  scitbx.random.set_random_seed(1234)
+  rand_norm = scitbx.random.normal_distribution(mean=0, sigma=SIM.mosaic_spread_deg * math.pi/180.)
+  g = scitbx.random.variate(rand_norm)
+  mosaic_rotation = g(SIM.mosaic_domains)
+  for m in mosaic_rotation:
+    site = col(mersenne_twister.random_double_point_on_sphere())
+    UMAT_nm.append( site.axis_and_angle_as_r3_rotation_matrix(m,deg=False) )
+  SIM.set_mosaic_blocks(UMAT_nm)
+  self.SIM = SIM
+
+
+ def set_rotation(self,rotation):
+
+  print("Determinant",rotation.determinant())
+  Amatrix_rot = (rotation * sqr(self.sfall_main.unit_cell().orthogonalization_matrix())).transpose()
+  prefix = "coarse_grain"
+  print("RAND_ORI", prefix)
+  print(Amatrix_rot[0],Amatrix_rot[1],Amatrix_rot[2])
+  print(Amatrix_rot[3],Amatrix_rot[4],Amatrix_rot[5])
+  print(Amatrix_rot[6],Amatrix_rot[7],Amatrix_rot[8])
+  print()
+
+  self.SIM.Amatrix_RUB = Amatrix_rot
+  #workaround for failing init_cell, use custom written Amatrix setter
+  Amat = sqr(self.SIM.Amatrix).transpose() # recovered Amatrix from SIM
+  from cctbx import crystal_orientation
+  Ori = crystal_orientation.crystal_orientation(Amat, crystal_orientation.basis_type.reciprocal)
+  print("Python unit cell from SIM state",Ori.unit_cell())
+  self.MM = self.SIM.get_mosaic_domains_abc()
+ def get_average_abc(self):
+   av_a = col((0.,0.,0.))
+   av_b = col((0.,0.,0.))
+   av_c = col((0.,0.,0.))
+   for val in self.MM:
+     av_a += col((val[0],val[1],val[2]))
+     av_b += col((val[3],val[4],val[5]))
+     av_c += col((val[6],val[7],val[8]))
+   av_a/=len(self.MM); av_b/=len(self.MM); av_c/=len(self.MM);
+   #further modify the average vector so it has the original length
+   La = col((self.MM[0][0],self.MM[0][1],self.MM[0][2])).length()
+   Lb = col((self.MM[0][3],self.MM[0][4],self.MM[0][5])).length()
+   Lc = col((self.MM[0][6],self.MM[0][7],self.MM[0][8])).length()
+   av_a *= La/(av_a.length())
+   av_b *= Lb/(av_b.length())
+   av_c *= Lc/(av_c.length())
+   return (av_a[0],av_a[1],av_a[2],av_b[0],av_b[1],av_b[2],av_c[0],av_c[1],av_c[2])
+
+if __name__ == "__main__":
+  for item in get_items():
+    abc = item['ABC']
+    print(abc[0],abc[1],abc[2])
+    print(abc[3],abc[4],abc[5])
+    print(abc[6],abc[7],abc[8])
+    serial_no = item['serial_no']
+    rotation = tst_all(serial_no)
+    M = nanoBragg_mock()
+    M.set_rotation(rotation)
+    print (M.get_average_abc())
+    #from IPython import embed; embed()
+    exit()
+
---
+++ work_pre_experiment/step5_ang_misset_to_fine_detail.py
@@ -0,0 +1,116 @@
+from __future__ import division, absolute_import, print_function
+from scitbx.array_family import flex
+from scitbx.matrix import sqr
+#from dxtbx.format.Registry import Registry
+from cctbx import crystal_orientation
+import sys
+import glob,math
+import dials
+
+# some parameters
+json_glob = "/net/dials/raid1/sauter/LS49_integ_allrestr/idx*.img_integrated_experiments.json"
+image_glob = "/net/dials/raid1/sauter/LS49/step5_MPIbatch_0%05d.img.gz"
+global format_class
+
+detail_type = sys.argv[1] # coarse or fine
+integration_run = sys.argv[2] # LS49_integ_allrestr, LS49_integ_betarestr, LS49_integ_step5cori
+
+json_glob = json_glob.replace("LS49_integ_allrestr",integration_run)
+
+from LS49.work_pre_experiment.fine_detail_ground_truth import nanoBragg_mock
+Mock_nano = nanoBragg_mock()
+def get_items():
+  file_list = glob.glob(json_glob)
+  print ("There are %d items in the file list"%len(file_list))
+  format_class = None
+  for item in file_list:
+    serial_no = int(item[-37:-32])
+    image_file = image_glob%serial_no
+    #print (image_file)
+    if format_class is None:
+      #format_class = Registry.find(image_file)
+      from dxtbx.format.FormatSMVJHSim import FormatSMVJHSim
+      format_class = FormatSMVJHSim
+    i = format_class(image_file)
+    Z = i.get_smv_header(image_file)
+    ABC = Z[1]["DIRECT_SPACE_ABC"]
+    abc = tuple([float(a) for a in ABC.split(",")])
+
+    from dxtbx.model.experiment_list import ExperimentListFactory
+    EC = ExperimentListFactory.from_json_file(item,check_format=False)[0].crystal
+
+    print(abc[0],abc[1],abc[2])
+    print(abc[3],abc[4],abc[5])
+    print(abc[6],abc[7],abc[8])
+    from LS49.work_pre_experiment.fine_detail_ground_truth import tst_all
+    rotation = tst_all(serial_no)
+
+    Mock_nano.set_rotation(rotation)
+    FGabc = Mock_nano.get_average_abc()
+    print(FGabc[0],FGabc[1],FGabc[2])
+    print(FGabc[3],FGabc[4],FGabc[5])
+    print(FGabc[6],FGabc[7],FGabc[8])
+    if detail_type == "fine":
+      yield(dict(serial_no=serial_no,ABC=FGabc,integrated_crystal_model=EC))
+    elif detail_type == "coarse":
+      yield(dict(serial_no=serial_no,ABC=abc,integrated_crystal_model=EC))
+
+if __name__=="__main__":
+  pdb_lines = open("/net/dials/raid1/sauter/LS49/1m2a.pdb","r").read()
+  from LS49.sim.util_fmodel import gen_fmodel
+  GF = gen_fmodel(resolution=3.0,pdb_text=pdb_lines,algorithm="fft",wavelength=1.7)
+  CB_OP_C_P = GF.xray_structure.change_of_basis_op_to_primitive_setting() # from C to P
+  print(str(CB_OP_C_P))
+
+  icount=0
+  from scitbx.array_family import flex
+  angles=flex.double()
+  for stuff in get_items():
+    #print stuff
+    icount+=1
+    print("Iteration",icount)
+    # work up the crystal model from integration
+    direct_A = stuff["integrated_crystal_model"].get_A_inverse_as_sqr()
+    permute = sqr((0,0,1,0,1,0,-1,0,0))
+    sim_compatible = direct_A*permute # permute columns when post multiplying
+    from cctbx import crystal_orientation
+    integrated_Ori = crystal_orientation.crystal_orientation(sim_compatible, crystal_orientation.basis_type.direct)
+    #integrated_Ori.show(legend="integrated")
+
+    # work up the ground truth from header
+    header_Ori = crystal_orientation.crystal_orientation(stuff["ABC"], crystal_orientation.basis_type.direct)
+    header_Ori.show(legend="header_Ori")
+
+    C2_ground_truth = header_Ori.change_basis(CB_OP_C_P.inverse())
+    C2_ground_truth.show(legend="C2_ground_truth")
+
+    # align integrated model with ground truth
+    cb_op_align = integrated_Ori.best_similarity_transformation(C2_ground_truth,50,1)
+    aligned_Ori = integrated_Ori.change_basis(sqr(cb_op_align))
+    aligned_Ori.show(legend="integrated, aligned")
+    print("alignment matrix", cb_op_align)
+
+    U_integrated = aligned_Ori.get_U_as_sqr()
+    U_ground_truth = C2_ground_truth.get_U_as_sqr()
+
+    missetting_rot = U_integrated * U_ground_truth.inverse()
+    print("determinant",missetting_rot.determinant())
+    from libtbx.test_utils import approx_equal
+    assert approx_equal(missetting_rot.determinant(),1.0)
+    angle,axis = missetting_rot.r3_rotation_matrix_as_unit_quaternion().unit_quaternion_as_axis_and_angle(deg=True)
+    #angles.append(angle)
+    #print "Item %5d angular offset is %8.5f deg."%(icount,angle)
+
+    # now calculate the angle as mean a_to_a,b_to_b,c_to_c
+    aoff = aligned_Ori.a.angle(C2_ground_truth.a,deg=True)
+    boff = aligned_Ori.b.angle(C2_ground_truth.b,deg=True)
+    coff = aligned_Ori.c.angle(C2_ground_truth.c,deg=True)
+    # solved:  the reason missetting_rot doesn't exactly align postref and ground_truth is
+    # that it's a monoclinic lattice, not orthorhombic.  Difference in the beta angle prevents exact alignment
+
+    hyp = flex.mean(flex.double((aoff,boff,coff)))
+
+    angles.append(hyp)
+    print("Item %5d serial_no %5d angular offset is %12.9f deg."%(icount,stuff["serial_no"],hyp))
+
+  print("RMSD", math.sqrt(flex.mean(angles*angles)))
