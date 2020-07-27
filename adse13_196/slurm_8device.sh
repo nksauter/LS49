b5e916475a6b8571f0a99e73fb37fa6599cb74a1
Nicholas K Sauter <nksauter@lbl.gov>
2020-07-27 09:25:53 -0700

    For FY20 milestone

---
+++ adse13_196/slurm_8device.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+#SBATCH -q special    # regular or special queue
+#SBATCH -N 1          # Number of nodes
+#SBATCH -t 01:10:00   # wall clock time limit
+#SBATCH -J test_gpu_job
+#SBATCH -L SCRATCH    # job requires SCRATCH files
+#SBATCH -C gpu
+#SBATCH -A m1759      # allocation
+#SBATCH -G 8          # devices per node
+#SBATCH -c 80         # total threads requested per node
+#SBATCH -o slurm%j.out
+#SBATCH -e slurm%j.err
+#SBATCH --mail-user=$USER@lbl.gov
+#SBATCH --mail-type=ALL
+#SBATCH --exclusive
+
+# -n, tasks to run; -N number of nodes; -c cpus per task;
+# n = N x tasks_per_node (should be 40 tasks per node for Cori-gpu)
+
+export N_SIM=240 # total number of images to simulate
+export ADD_SPOTS_ALGORITHM=cuda # cuda or JH or NKS
+export DEVICES_PER_NODE=8
+mkdir $SLURM_JOB_ID; cd $SLURM_JOB_ID
+date;ls
+srun -n 40 -c 2 libtbx.python $(libtbx.find_in_repositories LS49)/adse13_161/step5_batch.py
+date;ls
