d69277e6db326be8f30f254e0fb3e72f5967bce7
Nicholas K Sauter <nksauter@lbl.gov>
2020-08-20 11:44:23 -0700

    Remove quick test framework, is dead code

--- adse13_196/step5_pad.py
+++ adse13_196/step5_pad.py
@@ -386,25 +386,3 @@ def run_sim2smv(prefix,crystal,spectra,rotation,rank,quick=False,save_bragg=Fals
     data=img.get_raw_data(),path=prefix + ".cbf")
   SIM.free_all()
 
-def tst_all(quick=False,prefix="step5",save_bragg=False):
-  from LS49.spectra.generate_spectra import spectra_simulation
-  SS = spectra_simulation()
-  iterator = SS.generate_recast_renormalized_images(20,energy=7120.,total_flux=1e12)
-
-  #
-  C = microcrystal(Deff_A = 4000, length_um = 4., beam_diameter_um = 1.0) # assume smaller than 10 um crystals
-
-  if quick: prefix_root=prefix + "_%06d"
-  else: prefix_root=prefix + "poly_%06d"
-
-  Nimages = 1# 10000
-  from LS49 import legacy_random_orientations
-  random_orientations = legacy_random_orientations(Nimages)
-  for iteration in range(Nimages):
-    file_prefix = prefix_root%iteration
-    rand_ori = sqr(random_orientations[iteration])
-    run_sim2smv(prefix = file_prefix,crystal = C,spectra=iterator,rotation=rand_ori,quick=quick,rank=0,save_bragg=save_bragg)
-
-if __name__=="__main__":
-  tst_all()
-  print("OK")
