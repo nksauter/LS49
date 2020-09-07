89fb1844d7f9fd38fc0c125b3d9c651bf6325a0f
Nicholas K Sauter <nksauter@lbl.gov>
2020-09-07 12:19:18 -0700

    Remove cProfile from default, run faster.
    Separate the concepts of python_profiling and per_rank_logging.
    Removing cProfile from the default operation cuts down on overhead,
    and this efficiency gain is needed for the adse13-196 milestone.
    Change all batch submission scripts to reflect new variables.

--- adse13_196/linux_1node.sh
+++ adse13_196/linux_1node.sh
@@ -2,11 +2,13 @@
 #mkdir results
 #cd results
 
-export LOG_BY_RANK=1 # Use Aaron's profiler/rank logger
+export LOG_BY_RANK=1 # Use Aaron's rank logger
+export RANK_PROFILE=0 # 0 or 1 Use cProfiler, default 1
 export OMP_NUM_THREADS=60
 export N_SIM=40 # total number of images to simulate
 export ADD_SPOTS_ALGORITHM=NKS # cuda or JH or NKS
 export DEVICES_PER_NODE=1
-date;pwd;ls
+echo "jobstart $(date)";pwd;ls
 libtbx.python $(libtbx.find_in_repositories LS49)/adse13_161/step5_batch.py
+echo "jobend $(date)";pwd
 
