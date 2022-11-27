e90034e67ceaf3c5ef64af4f60c22184f6df655f
Nicholas K Sauter <nksauter@lbl.gov>
2022-11-26 19:46:26 -0800

    Slightly more informative MPI test-gives nodename

--- adse13_196/test_mpi.py
+++ adse13_196/test_mpi.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from mpi4py import MPI
+import os
 mpi_rank = MPI.COMM_WORLD.Get_rank()
 mpi_size = MPI.COMM_WORLD.Get_size()
-print(mpi_rank, "of", mpi_size)
+print(mpi_rank, "of", mpi_size, "on node", os.environ.get("SLURMD_NODENAME",""))
