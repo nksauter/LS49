537868d50ea78b99c06ef267e27c017a745e9cff
vganapati <vganapa1@swarthmore.edu>
2023-02-27 16:35:22 -0800

    parameter sweeps

--- LY99/spread1/step11_params9.sh
+++ LY99/spread1/step11_params9.sh
@@ -1,6 +1,16 @@
-export OUTPUT_FOLDER=small_example_out
+#!/bin/bash -l
+#SBATCH -N 48               # Number of nodes on Perlmutter
+#SBATCH -J test_spread
+#SBATCH -L SCRATCH          # job requires SCRATCH files
+#SBATCH -A lcls_g           # allocation
+#SBATCH -C gpu
+#SBATCH -q regular          # regular queue
+#SBATCH -t 02:00:00         # wall clock time limit
+#SBATCH --ntasks-per-gpu=1
+#SBATCH -o %j.out
+#SBATCH -e %j.err
 
-mkdir -p $OUTPUT_FOLDER; cd $OUTPUT_FOLDER
+mkdir -p $SLURM_JOB_ID; cd $SLURM_JOB_ID
 
 export CCTBX_NO_UUID=1
 export DIFFBRAGG_USE_CUDA=1
@@ -17,10 +27,11 @@ export XFEL_CUSTOM_WORKER_PATH=$MODULES/psii_spread/merging/application # User m
 export WERK=/global/cfs/cdirs/lcls/sauter/LY99/
 
 echo "
-dispatch.step_list = input annulus
+dispatch.step_list = input balance annulus
 input.path=$WERK/ready2/3724210/out
-input.experiments_suffix=trial8_scenario3A_000000.expt
-input.reflections_suffix=trial8_scenario3A_000000.refl
+input.experiments_suffix=.expt  # switch back for production
+input.reflections_suffix=.refl  # switch back for production
+input.parallel_file_load.balance=global2
 input.keep_imagesets=True
 input.read_image_headers=False
 input.persistent_refl_cols=shoebox
@@ -32,6 +43,7 @@ input.persistent_refl_cols=xyzobs.mm.value
 input.persistent_refl_cols=xyzobs.mm.variance
 input.persistent_refl_cols=delpsical.rad
 input.persistent_refl_cols=panel
+input.parallel_file_load.method=uniform
 
 scaling.model=$WERK/reference/6ydi.pdb
 scaling.unit_cell=107.00  107.00  304.01  90.00  90.00  90.00
@@ -43,15 +55,15 @@ filter.unit_cell.cluster.covariance.file=$WERK/reference/covariance_run145_cells
 filter.unit_cell.cluster.covariance.component=0
 
 merging.d_max=None
-merging.d_min=2.75
+merging.d_min=2.5
 
-statistics.annulus.d_max=3.0
-statistics.annulus.d_min=2.75
+statistics.annulus.d_max=4.0
+statistics.annulus.d_min=2.5
 
 spread_roi.enable=True
 # spread_roi.strong=1.0 # only use for initial annulus definition, not subsequent
 
-output.log_level=1 # 0 = stdout stderr, 1 = terminal
+output.log_level=0 # 0 = stdout stderr, 1 = terminal
 output.output_dir=out
 output.prefix=trial8_scenario3A
 output.save_experiments_and_reflections=True
@@ -92,16 +104,18 @@ exafel{
 exafel.metal=MMO2
 sauter20.LLG_evaluator.enable_plot=True
 sauter20.LLG_evaluator.title=tell
-sauter20.LLG_evaluator.restraints.fp.sigma=0.04
-sauter20.LLG_evaluator.restraints.fdp.sigma=0.08
+sauter20.LLG_evaluator.restraints.fp.mean=None
+sauter20.LLG_evaluator.restraints.fp.sigma=None
+sauter20.LLG_evaluator.restraints.fdp.mean=None
+sauter20.LLG_evaluator.restraints.fdp.sigma=None
 sauter20.LLG_evaluator.restraints.kramers_kronig.use=True
-sauter20.LLG_evaluator.restraints.kramers_kronig.pad=100
-sauter20.LLG_evaluator.restraints.kramers_kronig.trim=10
-sauter20.LLG_evaluator.restraints.kramers_kronig.weighting_factor=10000.0
-sauter20.LLG_evaluator.max_calls=2
+sauter20.LLG_evaluator.restraints.kramers_kronig.pad=200
+sauter20.LLG_evaluator.restraints.kramers_kronig.trim=0
+sauter20.LLG_evaluator.restraints.kramers_kronig.weighting_factor=10.0
+sauter20.LLG_evaluator.max_calls=30
 trumpet.plot_all.enable=False
 trumpet.plot_all.savepng=True
 " > trial8.phil
 echo "jobstart $(date)";pwd
-cctbx.xfel.merge trial8.phil
+srun -n 192 -c 4 cctbx.xfel.merge trial8.phil
 echo "jobend $(date)";pwd
